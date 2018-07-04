c523b9a505c6585a9ef4f2b01f2ec7b0ea931099
Craig J. Bass <craig@madetech.co.uk>
2018-07-04 16:50:21 +0100

    Implement scoring for deuce/advantage for player one

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,42 +1,80 @@
+class Player
+  attr_reader :name, :points
+
+  def initialize(name)
+    @name = name
+    @points = 0
+  end
+
+  def won_point!
+    @points += 1
+  end
+
+  def won_against?(other_player)
+    return false if @points < 4
+    return true if @points == 4 && other_player.points < 4
+
+    @points - other_player.points >= 2
+  end
+end
+
 class Tennis
   def initialize(player_one_name, player_two_name)
-    @player_one_name = player_one_name
-    @player_two_name = player_two_name
-    @point = 0
-    @point2 = 0
+    @player_one = Player.new(player_one_name)
+    @player_two = Player.new(player_two_name)
   end
 
   def won_point(player_name)
-    if @player_one_name == player_name
-      @point += 1
-    else
-      @point2 += 1
-    end
+    player = find_player(player_name)
+    player.won_point!
   end
 
   def score
-    return win(@player_one_name) if player_one_wins?
-    return win(@player_two_name) if player_two_wins?
+    return 'Deuce' if deuce?
+    return "Advantage #{@player_one.name}" if advantage_player_one?
+    return a_win_for(the_winning_player) unless no_winner?
 
     side_by_side_points
   end
 
   private
 
-  def player_one_wins?
-    @point == 4
+  def deuce?
+    players.map(&:points).sum == 6
   end
 
-  def player_two_wins?
-    @point2 == 4
+  def advantage_player_one?
+    difference = @player_one.points - @player_two.points == 1
+    at_least_deuce = players.map(&:points).reject { |p| p < 3 }.count == 2
+    difference && at_least_deuce
   end
 
-  def win(player_name)
-    "#{player_name} wins"
+  def players
+    [@player_one, @player_two]
+  end
+
+  def find_player(player_name)
+    players.select { |p| p.name == player_name }
+           .first
+  end
+
+  def the_winning_player
+    pairs = players.zip(players.reverse)
+    pairs.select do |(a, b)|
+      a.won_against?(b)
+    end.dig(0, 0)
+  end
+
+  def no_winner?
+    the_winning_player.nil?
+  end
+
+  def a_win_for(player)
+    "#{player.name} wins"
   end
 
   def side_by_side_points
-    "#{point(@point)}-#{point(@point2)}"
+    "#{point(@player_one.points)}-#{point(@player_two.points)}"
   end
 
   def point(point)
@@ -83,6 +121,7 @@ describe Tennis do
     expect_score('Thirty-Love', points: [1] * 2)
     expect_score('Forty-Love', points: [1] * 3)
     expect_score('Jane wins', points: [1] * 4)
+    expect_score('Jane wins', points: [1, 2, 1, 2, 1, 2, 1, 1])
   end
 
   it 'can use different winner names for player one' do
@@ -106,6 +145,25 @@ describe Tennis do
 
   it 'can score when both players have won points' do
     expect_score('Thirty-Thirty', points: [1, 2, 1, 2])
+  end
+
+  it 'can score deuce' do
+    expect_score('Deuce', points: [1, 2, 1, 2, 1, 2])
+  end
+
+  it 'can score advantage for Jane' do
+    expect_score('Advantage Jane', points: [1, 2, 1, 2, 1, 2, 1])
+    expect_score('Advantage Jane', points: [1, 2, 1, 2, 1, 2, 1, 2, 1])
+  end
+
+  it 'can score advantage for player one with different name' do
+    @player_one_name = 'Merry'
+    expect_score('Advantage Merry', points: [1, 2, 1, 2, 1, 2, 1])
+    expect_score('Advantage Merry', points: [1, 2, 1, 2, 1, 2, 1, 2, 1])
+  end
+
+  xit 'can score win for Bill' do
+    expect_score('Advantage Bill', points: [2, 1, 2, 1, 2, 1, 2])
   end
 end
 # rubocop:enable Metrics/BlockLength
